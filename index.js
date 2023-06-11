785992c5f267f1d4a189c8c3a8d58213a843861d
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-06-11 18:08:41 -0300

    remove dotenv

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
@@ -9,7 +8,7 @@ app.use(express.json());
 app.use(routes);
 
 const env = process.env.NODE_ENV || "development";
-const { PORT } = config().parsed;
+const { PORT } = process.env;
 
 app.listen({ host: "0.0.0.0", port: Number(PORT) || 3333 }, () => {
   console.log(process.env);
