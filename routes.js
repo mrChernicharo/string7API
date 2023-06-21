6fe40da917c1737f1011f2375b9d0645c37378a6
Felipe Chernicharo <string7dev@gmail.com>
2023-06-21 19:53:17 -0300

    add video_url and image_urls to projects response

--- routes.js
+++ routes.js
@@ -23,14 +23,49 @@ routes.use('/skills', async (req, res) => {
 
 routes.use('/projects', async (req, res) => {
 	try {
-		const projects = await sql`
-      select proj.*, videos.url as video_url 
-      from projects as proj
-      left join project_videos as videos 
-      on videos.project_id = proj.id;
-    `;
+		const projectImgs = await sql`
+			select * from project_images
+		`;
 
-		return res.json({ projects });
+		const projectVideos = await sql`
+			select * from project_videos
+		`;
+
+		const projects = await sql`
+			select * from projects
+		`;
+
+		const videosByProj = {};
+		for (const video of projectVideos.values()) {
+			videosByProj[video.project_id] = video.url;
+		}
+
+		const imgsByProj = {};
+		for (const img of projectImgs.values()) {
+			if (!(img.project_id in imgsByProj)) {
+				imgsByProj[img.project_id] = [];
+			}
+			imgsByProj[img.project_id].push(img.url);
+		}
+
+		const finalData = projects.map(proj => {
+			return {
+				...proj,
+				image_urls: imgsByProj[proj.id],
+				video_url: videosByProj[proj.id],
+			};
+		});
+
+		return res.json({
+			projects: finalData,
+		});
+
+		// 	const projects = await sql`
+		//   select proj.*, videos.url as video_url
+		//   from projects as proj
+		//   left join project_videos as videos
+		//   on videos.project_id = proj.id;
+		// `;
 	} catch (err) {
 		return res.status(400).json({ error: err.message });
 	}
